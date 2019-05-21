439a615d14226252cbe574d74df588625399cddb
Jen W <jsc525@gmail.com>
2019-05-21 01:51:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-if (sayHiToGrandma(string) === lowercase) {
+function sayHiToGrandma(string.toLowerCase) {
   return string.toEqual("I can't hear you!")
 }
 
